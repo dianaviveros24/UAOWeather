96afa5a5a15daa6aabf91d7b1e384c52b11236eb
Diana Viveros <mayce998@gmail.com>
2021-02-08 20:12:24 -0500

    uso de datos y definición de estado

--- src/Components/WeatherInfo.js
+++ src/Components/WeatherInfo.js
@@ -2,7 +2,6 @@ import React from "react";
 import WeatherExtra from "../Components/WeatherExtra /WeatherExtra";
 import WeatherTemp from "./../Components/WeatherTemp/WeatherTemp";
 
-
 const WeatherInfo = ({data: {temp, weatherState, humidity, wind}}) => (
     <div className="o-weather-information">
         <div>
